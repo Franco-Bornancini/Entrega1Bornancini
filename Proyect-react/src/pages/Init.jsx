c0aee455fec46c89d015e5e8985596ef3d1c4c8a
Franco <bornancinifranco72@gmail.com>
2025-11-06 12:51:02 -0300

    add css

---
+++ Proyect-react/src/pages/Init.jsx
@@ -0,0 +1,16 @@
+import '../styles/Init.css'
+
+
+
+const Init = () => {
+
+    return (
+        <div className="wrapper">
+            <div className="container">
+            <h1 className="title">Primera Entrega</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Init
